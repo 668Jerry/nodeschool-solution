c1d196d6cbb65d3bf31f59aa1ebea862cbe5e24d
AWS Server <668Jerry@gmail.com>
2015-05-23 15:37:25 +0800

    http_file_server

---
+++ http_file_server.js
@@ -0,0 +1,11 @@
+/**
+ * Created by lololol on 5/23/15.
+ */
+
+var http = require('http')
+var fs = require('fs')
+var server = http.createServer(function (req, res) {
+    // request handling logic...
+    fs.createReadStream(process.argv[3]).pipe(res);
+})
+server.listen(process.argv[2]);
